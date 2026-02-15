1b1b36cb6554c8a1a280e63495843e5697ad7dad
jasmaa <jasonmaa3955@gmail.com>
2026-02-15 09:52:36 -0500

    Format whitespace and add black

--- build.py
+++ build.py
@@ -6,24 +6,23 @@ import frontmatter
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(
-    loader=FileSystemLoader(searchpath='./templates'),
+    loader=FileSystemLoader(searchpath="./templates"),
 )
 
 
 def parse_section(section_path: str) -> str:
-    """Renders section.
-    """
+    """Renders section."""
     section = frontmatter.load(section_path)
     return {
-        'title': section.get('title'),
-        'start_date': section.get('start_date'),
-        'end_date': section.get('end_date'),
-        'content': marko.convert(section.content),
+        "title": section.get("title"),
+        "start_date": section.get("start_date"),
+        "end_date": section.get("end_date"),
+        "content": marko.convert(section.content),
     }
 
-def render_site(sections_dir: str = './sections'):
-    """Renders site.
-    """
+
+def render_site(sections_dir: str = "./sections"):
+    """Renders site."""
     # Build sections
     section_fnames = os.listdir(sections_dir)
     section_fnames.sort()
@@ -33,30 +32,29 @@ def render_site(sections_dir: str = './sections'):
         sections.append(parse_section(os.path.join(sections_dir, fname)))
 
     # Render index
-    template = env.get_template('index.html')
+    template = env.get_template("index.html")
     return template.render(sections=sections)
 
 
-def build(build_dir: str = './build'):
-    """Builds site.
-    """
+def build(build_dir: str = "./build"):
+    """Builds site."""
 
     # Create build directory
     if not os.path.exists(build_dir):
         os.mkdir(build_dir)
 
     # Render site
-    with open(os.path.join(build_dir, 'index.html'), 'w') as f:
+    with open(os.path.join(build_dir, "index.html"), "w") as f:
         f.write(render_site())
 
     # Copy static directory
-    build_static_dir = os.path.join(build_dir, './static')
+    build_static_dir = os.path.join(build_dir, "./static")
     if os.path.exists(build_static_dir):
         shutil.rmtree(build_static_dir)
-    shutil.copytree('./static', build_static_dir)
+    shutil.copytree("./static", build_static_dir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Starting build...")
     build()
     print("Done!")
